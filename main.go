cee4bddb61220c8c8c4e27f5050e43216b7fd01f
Dmitriy <dima.pozdneev@yandex.ru>
2024-05-05 12:20:15 +0300

    Первый коммит

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func proizvodnaya(x float64) float64 {
+	return (((3 * x * x) / 5) - (2 * x) - 1)
+}
+func sama_function(x float64) float64 {
+	return (((x * x * x) / 5) - (x * x) + x)
+}
+func main() {
+
+}
